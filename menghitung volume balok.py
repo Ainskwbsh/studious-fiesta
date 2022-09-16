2068a56123edd07f7c30bad2230d82f07cace081
Ainskwbsh <113685052+Ainskwbsh@users.noreply.github.com>
2022-09-16 17:50:34 +0700

    Add files via upload

---
+++ menghitung volume balok.py
@@ -0,0 +1,9 @@
+print("menghitung volume balok")
+
+p = int(input("masukan panjangnya ="))
+l = int(input("masukan lebarnya   ="))
+t = int(input("masukan tingginya  ="))
+
+volume = p*l*t
+
+print("volume baloknya adalah = ", volume)
---
+++ menghitung volume kubus.py
@@ -0,0 +1,7 @@
+print("menghitung volume kubus")
+
+r = int(input("masukan rusuknya="))
+
+volume = r*r*r
+
+print("volume kubus adalah = ",volume)
